8328d5108b7d16b7fe92c07a08880d0bea9f1ba9
Chris Scoffield <CScoffield@bell-integration.com>
2021-01-26 10:47:02 +0000

    added option to pass starting day2

--- days_offset.py
+++ days_offset.py
@@ -15,23 +15,38 @@ So if i passwd an offset of 7 this would be a Sunday, a 5 a Friday, a 13 a Satur
 
 import sys
 
-def solution  (S):
+def solution  (D,S):
+
+    # define tuple array - use day as day-1 so that it works easily with the modulus
+    days = ([0,"Sun"],[1,'Mon'],[2,"Tue"],[3,"Wed"],[4,"Thu"],[5,"Fri"],[6,"Sat"])
 
    # Work out which day equates to this
     which_day = S % 7
     newday=0
 
-    # define tuple array - use day as day-1 so that it works easily with the modulus
 
-    days = ([0,"Sun"],[1,'Mon'],[2,"Tue"],[3,"Wed"],[4,"Thu"],[5,"Fri"],[6,"Sat"])
+    # Find the starting day Integer
+    # (If Sun, add 7)
+    # Then add the day offset and get the modulus
+    # in order to determine the final day
+    for day in days:
+        if day[1] == D:
+            starting_day = day[0]
+            if starting_day == 0:
+                starting_day = 7
+            which_day = (S+starting_day)%7
+            break
+
+    # Now we have the final day, work out the day name
     for day in days:
         if day[0] == which_day:
             return day[1]
 
-def main (S):
-    ans = solution(S)
+def main (D,S):
+    ans = solution(D,S)
     print(ans)
 
 
 if __name__ == "__main__":
-    main(int(sys.argv[1])
+    main(sys.argv[1],int(sys.argv[2]))
+
